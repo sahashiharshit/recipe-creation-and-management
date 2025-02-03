0da05765e24edeaa0d6a834b29443333d37ba424
Harshit Sah <harshitsah96@gmail.com>
2025-02-03 16:31:05 +0530

    Changes

--- routes/review.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-
-const reviewRoutes = Router();
-//Edit a review
-reviewRoutes.put('/:reviewId',(req,res)=>{});
-//Delete a review
-reviewRoutes.delete('/:reviewId',(req,res)=>{});
-export default reviewRoutes;
