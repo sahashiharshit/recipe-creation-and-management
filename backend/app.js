119e82c61251c483180a5ee9049f013076d5a864
Harshit Sah <harshitsah96@gmail.com>
2025-02-28 17:18:01 +0530

    new

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 app.use(
   cors({
-   origin:["http://localhost:5173","https://recipe-management-lovat.vercel.app"],
+   origin:"https://recipe-management-lovat.vercel.app",
     credentials:true,
     methods: "GET,POST,PUT,DELETE,OPTIONS",
     allowedHeaders: "Authorization, Content-Type",
