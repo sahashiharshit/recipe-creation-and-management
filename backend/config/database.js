2c5a06a2bd2cba1e57d3a7b162ccc587fde613ac
Harshit Sah <harshitsah96@gmail.com>
2025-02-28 14:39:55 +0530

    update2

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,6 +6,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     host: process.env.DB_HOST,
     port:process.env.DB_PORT,
     dialect: 'postgres',
+    dialectOptions:{ssl:false},
     logging: false,
 });
 
