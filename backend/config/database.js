bf2c2c3de39799df2270fbcc7b7dd8439a7ce334
Harshit Sah <harshitsah96@gmail.com>
2025-02-28 14:46:26 +0530

    update 4

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,22 +1,32 @@
-import { Sequelize } from 'sequelize';
-import {configDotenv} from 'dotenv';
+import { Sequelize } from "sequelize";
+import { configDotenv } from "dotenv";
 configDotenv();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
     host: process.env.DB_HOST,
-    port:process.env.DB_PORT,
-    dialect: 'postgres',
-    dialectOptions:{ssl:false},
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
     logging: false,
-});
+  }
+);
 
 const connectDB = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Database connected successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
+  try {
+    await sequelize.authenticate();
+    console.log("Database connected successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
 };
 
 connectDB();
