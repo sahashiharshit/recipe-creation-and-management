c1466b9f0e5a1abae2041f6df7b5849cf4e1de8b
Harshit Sah <harshitsah96@gmail.com>
2025-03-22 16:17:53 +0530

    final test 5

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,2 +1,5 @@
- export const API_BASE_URL="https://recipesapp.duckdns.org";
+export const API_BASE_URL =
+window.location.origin === "https://admin-recipesapp.duckdns.org"
+  ? "https://admin-recipesapp.duckdns.org"
+  : "https://recipesapp.duckdns.org";
 //export const API_BASE_URL="http://localhost:5000";
